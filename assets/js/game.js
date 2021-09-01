5ce718c5b715136ac69b961c7d855228567ba4e7
Lissette Vega <lissettevega@Lissettes-MacBook-Pro.local>
2021-09-01 13:52:59 -0400

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,6 +69,12 @@ var fight = function(enemyName) {
          
     }
 };
+//  function to start a new game
+var startGame = function() {
+    // reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
 
 for(var i = 0; i <enemyNames.length;i++) {
     // if player is still alive, keep fighting
@@ -92,5 +98,32 @@ for(var i = 0; i <enemyNames.length;i++) {
   else {
     window.alert('You have lost your robot in battle! Game Over!');
     break;
-  }
-}
+        }
+    }
+    //  play again
+    endGame();
+};
+
+// funtion to end the entire game
+var endGame = function() {
+    //  if player is still alive, player wins!
+    if (playerHealth > 0) {
+        window.alert ("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else {
+        window.alert("You've lost your robot in battle.");
+    }
+    // ask player if they'd like to play game
+    var playAgainConfirm = confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        // restart the game
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+};
+
+//  start the game when the page loads
+startGame();
